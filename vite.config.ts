c6add4f370c0141712d7e62595bd4857eee4d160
Vishal Jeyam <vishaljeyam@icloud.com>
2026-02-24 11:08:52 -0600

    Fix Vite base for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5001", // 👈 Express backend
+        target: "http://localhost:5001",
         changeOrigin: true,
         secure: false,
       },
